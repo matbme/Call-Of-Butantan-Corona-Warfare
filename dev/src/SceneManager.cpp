07c752e3cfa8cae563658525a1963ca6ae813237
Mateus Melchiades <matbme@walkintosh.local>
2021-05-02 10:19:51 -0300

    MacOS compiling support + window title typo fix

--- dev/src/SceneManager.cpp
+++ dev/src/SceneManager.cpp
@@ -23,7 +23,15 @@ void SceneManager::initializeGraphics()
 
 	glfwInit();
 
-	window = glfwCreateWindow(width, height, "FInal Battle", nullptr, nullptr);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	window = glfwCreateWindow(width, height, "Final Battle", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
 	glfwSetWindowSizeCallback(window, resize);
